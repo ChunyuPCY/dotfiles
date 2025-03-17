7d8a1b131ff557dac8151580b586cac6a2e2349d
ChunyuPCY <18896534787@163.com>
2025-03-17 15:56:43 +0800

    feat update

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -3,7 +3,12 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 
--- keymap.set("n", "<A-h>", "^", { desc = "Go to start without blank" })
--- keymap.set("n", "<A-l>", "$", { desc = "Go to end without blank" })
-
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+
+keymap.del("n", "<S-h>")
+keymap.del("n", "<S-l>")
+
+keymap.set({ "n", "v" }, "<S-h>", "^", { desc = "Go to start without blank" })
+keymap.set({ "n", "v" }, "<S-l>", "$", { desc = "Go to end without blank" })
+keymap.set("n", "<Tab>h", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap.set("n", "<Tab>l", "<cmd>bnext<cr>", { desc = "Next Buffer" })
